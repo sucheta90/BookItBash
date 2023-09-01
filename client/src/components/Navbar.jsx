3f7136a8b8a7ec77cee1fe656eaf5bb75927e3ff
RyanTheScholar <Ryan.business.bowen@gmail.com>
2023-09-01 12:37:54 -0500

    added navbar.jsx, react-icons, logo and other nextUI files

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,118 @@
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+} from "@nextui-org/react";
+import { AcmeLogo } from "./AcmeLogo.jsx";
+import { useLocation } from "react-router-dom";
+import Logo from "../assets/BookItBash.png";
+
+export default function App() {
+  const currentPage = useLocation().pathname;
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const loggedOutItems = [
+    {
+      id: 1,
+      path: "/",
+      pathname: "Home",
+    },
+    {
+      id: 2,
+      path: "/Profile",
+      pathname: "Profile",
+    },
+    {
+      id: 3,
+      path: "/Login",
+      pathname: "Login",
+    },
+    {
+      id: 4,
+      path: "/Signup",
+      pathname: "Signup",
+    },
+  ];
+  const loggedInItems = [
+    {
+      id: 1,
+      path: "/",
+      pathname: "Home",
+    },
+    {
+      id: 2,
+      path: "/Profile",
+      pathname: "Profile",
+    },
+  ];
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          <Logo />
+          <p className="font-bold text-inherit">ACME</p>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        {loggedOutItems.map((navItem) => (
+          <NavbarItem
+            key={navItem.id}
+            className="purple-dark text-primary-500 font-bold"
+            id="Item"
+          >
+            <Link
+              to={navItem.path}
+              className={
+                currentPage === navItem.path ? "nav-link active" : "nav-link"
+              }
+            >
+              {navItem.pathname}
+            </Link>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarMenu>
+        {loggedOutItems.map((navItem) => (
+          <NavbarMenuItem key={navItem.id}>
+            <Link
+              color={
+                navItem.id === 2
+                  ? "primary"
+                  : navItem === loggedOutItems.length - 1
+                  ? "danger"
+                  : "foreground"
+              }
+              className="w-full"
+              href="#"
+              size="lg"
+            >
+              {navItem.pathname}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,88 @@
+// tailwind.config.js
+// tailwind.config.js
+const { nextui } = require("@nextui-org/react");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // ...
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+      },
+      gap: {
+        11: "3.5rem",
+        1: "1rem"
+      },
+      maxWidth: {
+        'desktop': '1820px',
+      },
+      padding: {
+        '5px': '5px',
+      },
+      margin: {
+        '1': '1%'
+      }
+    },
+    fontSize: {
+      sm: "0.8rem",
+      base: "1rem",
+      xl: "1.25rem",
+      "2xl": "1.563rem",
+      "3xl": "1.953rem",
+      "4xl": "2.441rem",
+      "5xl": "3.052rem",
+    },
+  },
+  darkMode: "class",
+  plugins: [nextui({
+    themes: {
+      "purple-dark": {
+        // this is a theme from nextUI with its own spin on the primary colors
+        colors: {
+          background: "0D001A",
+          foreground: "#ffffff",
+          primary: {
+            50: "#EBE0FF",
+            100: "#ECDAFE",
+            200: "#DAC7FF",
+            300: "#C7ADFF",
+            400: "#AC8BEE",
+            500: "#916DD5",
+            600: "#7151A9",
+            700: "#573D7F",
+            800: "#46325D",
+            900: "#3F3649",
+            DEFAULT: "#916DD5",
+            foreground: "#ffffff",
+          },
+          secondary: {
+            50: "#F2F168"
+          },
+          focus: "#F182F6",
+        },
+        layout: {
+          disabledOpacity: "0.3",
+          radius: {
+            small: "4px",
+            medium: "6px",
+            large: "8px",
+          },
+          borderWidth: {
+            none: "0",
+            small: "1px",
+            medium: "2px",
+            large: "3px",
+          },
+        },
+      },
+    },
+  })]
+}
+
