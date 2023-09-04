b627c0954cb292051a203bba12458906d6bdf7dc
Lilibeth Mejia <mejialily036@gmail.com>
2023-09-04 10:15:40 -0500

    added to TypeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,42 @@
-const resolvers = {};
+const { User, Event } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id }).populate('events');
+            }
+            throw AuthenticationError;
+        },
+    },
+
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, { username, password }) => {
+            const user = await User.findOne({ username });
+
+            if (!user) {
+                throw AuthenticationError;
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw AuthenticationError;
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+
+    },
+
+};
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,10 +1,25 @@
 const typeDefs = `
 type User {
     _id: ID
-    username: String
-    email: String
+    username: String!
+    email: String!
     password: String
     events: [Event]
+}
+
+type Event {
+    _id: ID
+    eventId: String!
+    name: String!
+    type: String!
+    venue: [Venue]
+    image: [Image]
+    ticketLink: String!
+    priceRangeMin: Int
+    priceRangeMax: Int
+    segment: Segment
+    genre: Genre
+    subGenre: SubGenre
 }
 
 type Venue {
@@ -27,94 +42,37 @@ type Image {
     fallback: Boolean
 }
 
-type Event {
-    _id: ID
-    eventId: String!
-    name: String!
-    type: String!
-    venue: [Venue]
-    image: [Image]
-    ticketLink: String!
-    priceRangeMin: Float
-    priceRangeMax: Float
-    segment: Segment
-    genre: Genre
-    subGenre: SubGenre
-}
-
-input EventData {
-    _id: ID
-    eventId: String!
-    name: String!
-    type: String!
-    venue: [Venue]
-    image: [Image]
-    ticketLink: String!
-    priceRangeMin: Float
-    priceRangeMax: Float
-    segment: Segment
-    genre: Genre
-    subGenre: SubGenre
-}
-
 type Segment {
     _id: ID
     segmentId: String!
     name: String
-    }
+}
 
 type Genre {
     _id: ID
     genreId: String!
     name: String
-    }
+}
 
 type SubGenre {
-     _id: ID
-    subGenreId: String!
-    name: String
-    }
-
-input Segment {
-    _id: ID
-    segmentId: String!
-    name: String
-    }
-
-input Genre {
-     _id: ID
-    genreId: String!
-    name: String
-    }
-
-input SubGenre {
     _id: ID
     subGenreId: String!
     name: String
-    }
-    
+}
 
 type Auth {
     token: ID!
     user: User
-}
-
+  }
 
 type Query {
-    user(username: String!): User
-    events(username: String): [Event]
-    event(Id: ID!): Event
     me: User
 }
 
 type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(username: String!, password: String!): Auth
-    createEvent(event: EventData): Event
-    removeUser(userId: ID!): User
-    removeEvent(id: ID!): Event
 }
-
 
 `
 
