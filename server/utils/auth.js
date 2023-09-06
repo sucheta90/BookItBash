3adecc9b6311c4d412d2efde5412900b71896147
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2023-09-06 14:12:46 -0400

    added dotEnv package to the server folder

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,20 +1,21 @@
-const { GraphQLError } = require('graphql');
-const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const { GraphQLError } = require("graphql");
+const jwt = require("jsonwebtoken");
 
-const secret = 'mysecretssshhhhhhh';
-const expiration = '2h';
+const secret = process.env.AUTH_SECRET;
+const expiration = "2h";
 
 module.exports = {
-  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+  AuthenticationError: new GraphQLError("Could not authenticate user.", {
     extensions: {
-      code: 'UNAUTHENTICATED',
+      code: "UNAUTHENTICATED",
     },
   }),
   authMiddleware: function ({ req }) {
     let token = req.body.token || req.query.token || req.headers.authorization;
 
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token.split(" ").pop().trim();
     }
 
     if (!token) {
@@ -25,7 +26,7 @@ module.exports = {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
-      console.log('Invalid token');
+      console.log("Invalid token");
     }
 
     return req;
